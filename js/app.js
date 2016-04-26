6494635c631a08f8c65037fc24caa84bdbe6d1c7
Andrew Eakett <aeakett@gmail.com>
2016-04-26 08:11:44 -0400

    start question ticker in random spot

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 $(document).foundation();
 $(function() {
    $('#questionTicker').vTicker();
+   var skipQuestions = Math.floor((Math.random() * 189));
+   for (var i = 0; i < skipQuestions; i++) {
+      $('#questionTicker').vTicker('next', {animate:false});
+   };
    $('#questionTickerContainer').addClass('qtcAnchor');
    FastClick.attach(document.body);
 });
